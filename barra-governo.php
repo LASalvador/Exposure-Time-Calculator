98eb23869b54e3607500f766f054cd1302a0d7a3
Lucas Almeida Salvador <lucas.salvador11358@gmail.com>
2018-09-28 14:52:02 -0300

    Star Project

---
+++ barra-governo.php
@@ -0,0 +1,12 @@
+
+
+<!--ORIGINAL
+- quando a barra do governo sofrer alterações, deve-se entrar através de um navegador no link  abaixo (//barra.brasil.gov.br/barra.js) e copiar o código que aparece para o arquivo /include/barra_governo.js
+-->
+
+<div id="barra-brasil"> 
+	<ul id="menu-barra-temp">
+		<li><a href="http://brasil.gov.br">Portal do Governo Brasileiro</a></li> 
+		<li><a href="http://epwg.governoeletronico.gov.br/barra/atualize.html">Atualize sua Barra de Governo</a></li>
+	</ul>
+</div>
