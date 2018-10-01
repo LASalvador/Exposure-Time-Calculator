3169cdd88064e99cd8049eec77cd14e1d3148b57
Lucas Almeida Salvador <lucas.salvador11358@gmail.com>
2018-10-01 18:41:30 -0300

    Add the page responsible to show the outputs

---
+++ output.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Output</title>
+	<meta charset="utf-8">
+	<style>
+	.output{
+			background-color: #dddddd;
+		}
+	section.values{
+		border-top: 1px solid #606060;
+	}
+	div{
+		margin-bottom: 800px;
+	}
+	</style>
+</head>
+<body>
+	<div>
+		
+	</div>
+	<!-- Begin Output View -->
+	<div class="output">
+		<!--Begin intermediate values -->
+		<section id="intermediate-values">
+				<h4>Quantum Efficiency</h4>
+					<h5></h5>
+				<h4>Gain</h4>
+					<h5></h5>
+				<h4>Readout Noise</h4>
+					<h5></h5>
+				<h4>Zero magnitude flux</h4>
+					<h5></h5>
+				<h4>Central wavelenght</h4>
+					<h5></h5>
+				<h4>Band width</h4>
+					<h5></h5>
+				<h4>Sky transparence</h4>
+					<h5></h5>
+				<h4>Plate scale</h4>
+					<h5></h5>
+
+		</section>
+		<!-- End intermediate values -->
+		<!-- Begin Final values -->
+		<section class="values">
+			<h4>Integration Time</h4>
+				<h5></h5>
+			<h4>Sigmpa P</h4>
+				<h5></h5>
+			<h4>Sigma V</h4>
+				<h5></h5>
+			<h4>Signal Noise Radio</h4>
+				<h5></h5>
+		</section>
+		<!--End Final values -->
+	</div>
+	<!-- End Output View -->
+</body> 
+</html>
