2df782d6fa6254bb6536fa900306048926df0b4d
Lucas Almeida Salvador <lucas.salvador11358@gmail.com>
2018-10-01 18:40:50 -0300

    JS resposible for the CCD Modal switch tabs

---
+++ js/modal.js
@@ -0,0 +1,14 @@
+document.getElementsByClassName("tablink")[0].click();
+function openCity(evt, cityName) {
+  var i, x, tablinks;
+  x = document.getElementsByClassName("city");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < x.length; i++) {
+    tablinks[i].classList.remove("w3-light-grey");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.classList.add("w3-light-grey");
+}
