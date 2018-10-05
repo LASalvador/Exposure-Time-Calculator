31d2eaf1979105078173a516a725b73c1d53e831
Lucas Almeida Salvador <lucas.salvador11358@gmail.com>
2018-10-05 15:52:36 -0300

    Add JSON files and Class JSON reader

---
+++ Model/ReaderJSON.php
@@ -0,0 +1,28 @@
+<?php
+	/**
+	 * This class read the JSON file and returns the requires values
+	 * @author: Lucas Almeida Salvador
+	 */
+	class ReaderJSON
+	{
+		function readQuantumEfficiency($CCDType, $FilterColor)
+		{
+			$arquivo = file_get_contents('../static/Quantum.json');
+			$json = json_decode($arquivo);
+
+			return $json->QuantumEfficiency->$CCDType->$FilterColor;
+		}
+		function readCCDvalues($ccdMode, $attribute)
+		{
+			$arquivo = file_get_contents('../static/ccd.json');
+			$json = json_decode($arquivo);
+			return $json->$ccdMode->$attribute;
+		}
+		function readFilter($filter, $attribute)
+		{
+			$arquivo = file_get_contents('../static/filter.json');
+			$json = json_decode($arquivo);
+			return  $json->$filter->$attribute;
+		}
+	}
+?>
