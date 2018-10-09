863a14e1749b8bbf796d26b6087e3c46d42d7578
Lucas Almeida Salvador <lucas.salvador11358@gmail.com>
2018-10-09 15:03:46 -0300

    Fixed errors in the setters methods

--- Model/Observation.php
+++ Model/Observation.php
@@ -10,7 +10,6 @@
  	private $sigmaV;
  	private $numberPhotons;
  	private $magnitude;
- 	private $flux;
  	private $signalNoiseRadio;
  	private $numberPixels;
  	private $radiusAperture;
@@ -21,40 +20,37 @@
 
  	function __construct()
  	{
- 		echo "fala galera to sendo criado";
  	}
- 	/*public function setSigmaP($snr, $nwp, $type)
+ 	public function setSigmaP($snr, $nwp, $type)
  	{
- 		if(type == 1)
+ 		if($type == 1)
  		{
- 			temp = (100/sqrt($snr)) *(1/nwp);
- 			$this->sigmaP = temp;
+ 			$temp = (100/sqrt($snr))*(1/$nwp);
+ 			$this->sigmaP = $temp;
  		}
  		else
  		{
- 			temp = (1/2) * ((100/sqrt($snr)) * (1/nwp));
- 			$this->sigmaP = temp;
+ 			$temp = (1/2) * ((100/sqrt($snr)) * (1/$nwp));
+ 			$this->sigmaP = $temp;
  		}
- 	}*/
+ 	}
  	public function getSigmaP()
  	{
  		return	$this->sigmaP;
  	}
- 	/*
  	public function setSigmaV($snr, $nwp)
  	{
- 		temp = (1/sqrt(2)) * (100/sqrt($snr)) * (1/nwp);
- 		$this->sigmaV = temp;
- 	}*/
+ 		$temp = (1/sqrt(2)) * (100/sqrt($snr)) * (1/$nwp);
+ 		$this->sigmaV = $temp;
+ 	}
  	public function getSigmaV()
  	{
  		return $this->sigmaV;
  	}
- 	/*
  	public function setNumberPhotons($q, $tSky, $f0, $filterWidth, $effectiveLenght, $dTel , $mag)
  	{
- 			$this->flux = $q * $tSky * K * $f0 * ($filterWidth/$effectiveLenght) * (dTel * dTel) * 10e-0.4*$mag;
- 	}*/
+ 			$this->numberPhotons = $q * $tSky * K * $f0 * ($filterWidth/$effectiveLenght) * ($dTel * $dTel) *  pow(10, 0.4*$mag);
+ 	}
  	public function getNumberPhotons()
  	{
  		return $this->numberPhotons;
@@ -67,26 +63,18 @@
  	{	
  		return $this->magnitude;
  	}
- 	/*public function setFlux($q, $tSky, $f0, $filterWidth, $effectiveLenght, $dTel , $mag)
- 	{
- 		$this->flux = $q * $tSky * K * $f0 * ($filterWidth/$effectiveLenght) * (dTel * dTel) * 10e-0.4*$mag;
- 	}*/
- 	public function getFlux($value='')
- 	{
- 		return $this->flux;
- 	}
- 	/*public function setSignalNoiseRadio($n, $t, $nPix, $nS, $nR, $g);
- 	{	
- 		$this->signalNoiseRadio = $n*$t/sqrt($n*$t+2*$nPix*($nS*$t+pow($nR,2)+pow(0.289, 2)*pow($g,2))); 	
- 	}*/
+ 	public function setSignalNoiseRadio($n, $t, $nPix, $nS, $nR, $g)
+ 	{		
+ 		$this->signalNoiseRadio = $n*$t/sqrt($n*$t+2*$nPix*($nS*$t + pow($nR,2) + pow(0.289, 2) * pow($g,2))); 	
+ 	}	
  	public function getSignalNoiseRadio()
  	{
  		return $this->signalNoiseRadio;
  	}
- 	/*public function setNumberPixels($rap)
+ 	public function setNumberPixels($rap)
  	{
- 		$this->numberPixels = PI * ($rap * $rap);
- 	}*/
+ 		$this->numberPixels = PI * pow($rap, 2);
+ 	}
  	public function getNumberPixels()
  	{
  		return $this->numberPixels;
@@ -99,18 +87,18 @@
  	{
  		return $this->radiusAperture;
  	}
- 	/*public function setTimeExposure($type,$t, $n, $snr, $n, $npix, $ns, $nr , $g);
+ 	public function setTimeExposure($type,$t, $n=0, $snr = 0, $npix = 0, $ns = 0, $nr = 0, $g = 0)
  	{
- 		if ($type == 1) 
+ 		if($type==1) 
  		{
  			$this->timeExposure = $t;
  		}
  		else
  		{
- 			$t = ($snr *( $n + 2* $npix *$ns) + sqrt(pow(-($snr *( $n + 2* $npix *$ns)), 2) - 4 * (pow($n,2)) * (-2*$npix* pow($snr,2) * (pow($nr,2) + pow(0.289, 2)*pow($g,2))))/2*pow($n, 2);
+ 			$t = ($snr *( $n + 2* $npix *$ns) + sqrt(pow(-($snr *( $n + 2* $npix *$ns)), 2) - 4 * (pow($n,2)) * (-2*$npix* pow($snr,2) * (pow($nr,2) + pow(0.289, 2)*pow($g,2))))/2*pow($n, 2));
  			$this->timeExposure = $t;
  		}
- 	}*/
+ 	}
  	public function getTimeExposure()
  	{
  		return $this->timeExposure;
