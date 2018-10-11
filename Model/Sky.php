f263f48b75acb82087999b5b98b7b93507bc4dd2
Lucas Almeida Salvador <lucas.salvador11358@gmail.com>
2018-10-11 18:30:10 -0300

    Add modified the method setNumberPhotons()

--- Model/Sky.php
+++ Model/Sky.php
@@ -8,16 +8,16 @@
 		private $numberPhotons;
 		private $transparencySky;
 		private $magnitudeSky;
-		
-		function __construct($transparencySky, $moonPhase)
+		function __construct($transparencySky, $moonPhase, $q , $f0, $filterWidth, $effectiveLenght, $dTel, $plateScale)
 		{
-			//$this->setNumberPhotons();
 			$this->setTransparencySky($transparencySky);
 			$this->setMagnitudeSky($moonPhase);
+			$this->setNumberPhotons($this->getMagnitudeSky(), $this->getTransparencySky(), $q, $f0, $filterWidth, $effectiveLenght, $dTel, $plateScale);
 		}
-		public function setNumberPhotons($numberPhotons)
+		public function setNumberPhotons($mSky, $tSky, $q , $f0, $filterWidth, $effectiveLenght, $dTel, $plateScale)
 		{
-			$this->numberPhotons = $numberPhotons;
+			$n = $q * $tSky * 1.18531e10* $f0 * ($filterWidth/$effectiveLenght) * pow($dTel, 2) * pow($plateScale, 2) * pow(10, -0.4*$mSky);
+				$this->numberPhotons = $n;
 		}
 		public function getNumberPhotons()
 		{
@@ -57,7 +57,7 @@
 				$this->magnitudeSky = 22;
 			}
 		}
-		public function getMagnitudeSKy()
+		public function getMagnitudeSky()
 		{
 			return $this->magnitudeSky;
 		}
