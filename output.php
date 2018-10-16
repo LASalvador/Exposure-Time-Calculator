ca0ce4fb0f7f6d58c768616e62383d259e1b7a62
Lucas Almeida Salvador <lucas.salvador11358@gmail.com>
2018-10-16 18:43:43 -0300

    update the file

--- output.php
+++ output.php
@@ -1,3 +1,4 @@
+ 
 <!DOCTYPE html>
 <html>
 <head>
@@ -10,48 +11,42 @@
 	section.values{
 		border-top: 1px solid #606060;
 	}
-	div{
-		margin-bottom: 800px;
-	}
 	</style>
 </head>
 <body>
-	<div>
-		
-	</div>
 	<!-- Begin Output View -->
-	<div class="output">
+	<div class="output" id="answer">
 		<!--Begin intermediate values -->
 		<section id="intermediate-values">
 				<h4>Quantum Efficiency</h4>
-					<h5></h5>
+					<h5><? session_start(); isset($_SESSION['quantumEfficiency'])? echo $_SESSION['quantumEfficiency']: ''; ?></h5>
 				<h4>Gain</h4>
-					<h5></h5>
+					<h5><? session_start(); isset($_SESSION['gain'])?$_SESSION['gain']: '';?></h5>
 				<h4>Readout Noise</h4>
-					<h5></h5>
+					<h5><? session_start(); isset($_SESSION['readoutNoise'])?$_SESSION['readoutNoise']: ''; ?></h5>
 				<h4>Zero magnitude flux</h4>
-					<h5></h5>
+					<h5><? session_start(); isset($_SESSION['fluxZero'])?$_SESSION['fluxZero']: ''; ?></h5>
 				<h4>Central wavelenght</h4>
-					<h5></h5>
+					<h5><? session_start(); isset($_SESSION['central'])?$_SESSION['central']: ''; ?></h5>
 				<h4>Band width</h4>
-					<h5></h5>
+					<h5><? session_start(); isset($_SESSION['band'])?$_SESSION['band']: ''; ?></h5>
 				<h4>Sky transparence</h4>
-					<h5></h5>
+					<h5><? session_start(); isset($_SESSION['sky'])?$_SESSION['sky']: ''; ?></h5>
 				<h4>Plate scale</h4>
-					<h5></h5>
+					<h5><? session_start(); isset($_SESSION['plateScale'])?$_SESSION['plateScale']: ''; ?></h5>
 
 		</section>
 		<!-- End intermediate values -->
 		<!-- Begin Final values -->
 		<section class="values">
 			<h4>Integration Time</h4>
-				<h5></h5>
+				<h5><? session_start(); isset($_SESSION['time'])?$_SESSION['time']: '';?></h5>
 			<h4>Sigmpa P</h4>
-				<h5></h5>
+				<h5><?session_start(); isset($_SESSION['sigmaP'])?$_SESSION['sigmaP']: ''; ?></h5>
 			<h4>Sigma V</h4>
-				<h5></h5>
+				<h5><?session_start(); isset($_SESSION['sigmaV'])?$_SESSION['sigmaV']: ''; ?></h5>
 			<h4>Signal Noise Radio</h4>
-				<h5></h5>
+				<h5><?session_start(); isset($_SESSION['snr'])?$_SESSION['snr']: ''; ?></h5>
 		</section>
 		<!--End Final values -->
 	</div>
