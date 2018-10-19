23397ea5c664858039c6471c86b19977f74b389e
Lucas Almeida Salvador <lucas.salvador11358@gmail.com>
2018-10-19 13:21:03 -0300

    Adding the output of calculations

--- output.php
+++ output.php
@@ -11,42 +11,69 @@
 	section.values{
 		border-top: 1px solid #606060;
 	}
+	h5{
+		color: #000000;
+	}
 	</style>
 </head>
 <body>
 	<!-- Begin Output View -->
-	<div class="output" id="answer">
+	<div class="output">
 		<!--Begin intermediate values -->
 		<section id="intermediate-values">
-				<h4>Quantum Efficiency</h4>
-					<h5><? session_start(); isset($_SESSION['quantumEfficiency'])? echo $_SESSION['quantumEfficiency']: ''; ?></h5>
-				<h4>Gain</h4>
-					<h5><? session_start(); isset($_SESSION['gain'])?$_SESSION['gain']: '';?></h5>
-				<h4>Readout Noise</h4>
-					<h5><? session_start(); isset($_SESSION['readoutNoise'])?$_SESSION['readoutNoise']: ''; ?></h5>
-				<h4>Zero magnitude flux</h4>
-					<h5><? session_start(); isset($_SESSION['fluxZero'])?$_SESSION['fluxZero']: ''; ?></h5>
-				<h4>Central wavelenght</h4>
-					<h5><? session_start(); isset($_SESSION['central'])?$_SESSION['central']: ''; ?></h5>
-				<h4>Band width</h4>
-					<h5><? session_start(); isset($_SESSION['band'])?$_SESSION['band']: ''; ?></h5>
-				<h4>Sky transparence</h4>
-					<h5><? session_start(); isset($_SESSION['sky'])?$_SESSION['sky']: ''; ?></h5>
-				<h4>Plate scale</h4>
-					<h5><? session_start(); isset($_SESSION['plateScale'])?$_SESSION['plateScale']: ''; ?></h5>
+			<?php
+				session_start();
+				$quantum = isset($_SESSION['quantumEfficiency'])? $_SESSION['quantumEfficiency']: 0; 
+				$gain = isset($_SESSION['gain'])?$_SESSION['gain']: 0;
+				$readoutNoise = isset($_SESSION['readoutNoise'])?$_SESSION['readoutNoise']: 0;
+				$fluxZero = isset($_SESSION['fluxZero'])?$_SESSION['fluxZero']: 0;
+				$central = isset($_SESSION['central'])?$_SESSION['central']: 0;
+				$band = isset($_SESSION['band'])?$_SESSION['band']: 0;
+				$sky = isset($_SESSION['sky'])?$_SESSION['sky']: 0;
+				$plateScale = isset($_SESSION['plateScale'])?$_SESSION['plateScale']: 0; 
+				
+				 echo "<h4>Quantum Efficiency</h4>";
+				 echo "<h5>".$quantum."</h5>";
+				 echo "<h4>Gain</h4>";
+				 echo "<h5>".$gain."</h5>";
+				 echo "<h4>Readout Noise</h4>";
+				 echo "<h5>".$readoutNoise."</h5>";
+				 echo "<h4>Zero magnitude flux</h4>";
+				 echo "<h5>".$fluxZero."</h5>";
+				 echo "<h4>Central wavelenght</h4>";
+				 echo "<h5>".$central."</h5>";
+				 echo "<h4>Band width</h4>";
+				 echo "<h5>".$band."</h5>";
+				 echo "<h4>Sky transparence</h4>";
+				 echo "<h5>".$sky."</h5>";
+				 echo "<h4>Plate scale</h4>";
+				 echo "<h5>".$plateScale."</h5>";
+			?>		
 
 		</section>
 		<!-- End intermediate values -->
 		<!-- Begin Final values -->
 		<section class="values">
-			<h4>Integration Time</h4>
-				<h5><? session_start(); isset($_SESSION['time'])?$_SESSION['time']: '';?></h5>
-			<h4>Sigmpa P</h4>
-				<h5><?session_start(); isset($_SESSION['sigmaP'])?$_SESSION['sigmaP']: ''; ?></h5>
-			<h4>Sigma V</h4>
-				<h5><?session_start(); isset($_SESSION['sigmaV'])?$_SESSION['sigmaV']: ''; ?></h5>
-			<h4>Signal Noise Radio</h4>
-				<h5><?session_start(); isset($_SESSION['snr'])?$_SESSION['snr']: ''; ?></h5>
+			<?php
+				session_start();
+				$time = isset($_SESSION['time']) ? $_SESSION['time'] : 0;
+				$sigmaP = isset($_SESSION['sigmaP']) ? $_SESSION['sigmaP']: 0;
+				$sigmaV = isset($_SESSION['sigmaV']) ? $_SESSION['sigmaV']: 0; 
+				$snr = isset($_SESSION['snr']) ? $_SESSION['snr']: 0;
+
+				echo "<h4>Integration Time</h4>";
+				echo "<h5>".$time."</h5>";
+				echo "<h4>Sigma P</h4>";
+				echo "<h5>".$sigmaP."</h5>";
+				echo "<h4>Sigma V</h4>";
+				echo "<h5>".$sigmaV."</h5>";
+				echo "<h4>Signal Noise Radio</h4>";
+				echo "<h5>".$snr."</h5>";
+
+				echo "Calculated by Exposure Time Calculator/IAGPOL<br>";
+				echo date ("d-m-Y")."<br>";
+				echo "link";
+			?>
 		</section>
 		<!--End Final values -->
 	</div>
