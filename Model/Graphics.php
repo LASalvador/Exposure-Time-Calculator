5d9eb7bd7fd55036db8e2ce2b170428e9a4959f6
Lucas Almeida Salvador <lucas.salvador11358@gmail.com>
2018-10-26 16:07:20 -0300

    Add the Graphics class

---
+++ Model/Graphics.php
@@ -0,0 +1,77 @@
+<?php
+	/**
+	 * This class do the graph dates and build the graph 
+	 * @author: Lucas Almeida Salvador
+	 */
+
+	include '../libs/phplot-6.2.0/phplot.php';
+	class Graphics 
+	{
+		private $observation;
+		private $sky;
+		private $instrument;
+		function __construct($observationObject,$skyObject, $instrumentObject)
+		{
+			
+			$this->setObservation($observationObject);
+			$this->setSky($skyObject);
+			$this->setInstrument($instrumentObject);
+		}
+
+		public function setObservation(Observation $observationObject)
+		{
+			$this->observation = $observationObject;
+		}
+		public function getObservation()
+		{	
+			return $this->observation;
+		}
+		public function setSky(Sky $skyObject)
+		{	
+			$this->sky = $skyObject;
+		}
+		public function getSky()
+		{	
+			return $this->sky;
+		}
+		public function setInstrument(Instrument $instrumentObject)
+		{
+			$this->instrument = $instrumentObject;
+		}
+		public function getInstrument()
+		{	
+			return $this->instrument;
+		}
+		public function generateValues($timeIntegration , $nwp)
+		{
+			if($timeIntegration>500)
+			{
+				$timeRange = $timeIntegration;
+			}
+			else
+			{
+				$timeRange = 500;
+			}
+
+
+			$data = array();
+			//defining in 1s
+			$this->getObservation()->setSignalNoiseRadio(1,$this->getObservation()->getNumberPhotons(), 1, $this->getObservation()->getNumberPixels(), $this->getSky()->getNumberPhotons(),$this->getInstrument()->getCCD()->getReadoutNoise(),$this->getInstrument()->getCCD()->getGain());
+
+				$this->getObservation()->setSigmaP(1,$this->getObservation()->getSignalNoiseRadio(),$nwp);
+
+			$data[] = array('',1, round($this->getObservation()->getSigmaP(),3) );
+
+			for ($time=20; $time <=$timeRange; $time+=20) 
+			{ 
+				$this->getObservation()->setSignalNoiseRadio(1,$this->getObservation()->getNumberPhotons(), $time, $this->getObservation()->getNumberPixels(), $this->getSky()->getNumberPhotons(),$this->getInstrument()->getCCD()->getReadoutNoise(),$this->getInstrument()->getCCD()->getGain());
+
+				$this->getObservation()->setSigmaP(1,$this->getObservation()->getSignalNoiseRadio(),$nwp);
+
+
+				$data[] = array('', $time, round($this->getObservation()->getSigmaP(), 3) );
+			}
+			return $data;
+		}
+	}
+?>
