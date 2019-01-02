3eb7fabbec758ffcae10939e4b5ab47a47dcdae3
Lucas Almeida Salvador <37023076+LASalvador@users.noreply.github.com>
2019-01-02 20:39:47 -0200

    Updated the comments

--- source/Model/Sky.php
+++ source/Model/Sky.php
@@ -9,27 +9,27 @@
 	{
 		/** Number Photons of Sky */
 		private $numberPhotons;
-		/** transparency of Sky */
+		/** Transparency of Sky */
 		private $transparencySky;
-		/** magnitude of Sky */
+		/** Magnitude of Sky */
 		private $magnitudeSky;
 		/** It's a factor to correct possible difference between this ETC results and the real measurements */
 		private $fCalib;
 
 		/**
-		* Constructor: sets up attributes of class
-		* @param String $transparencySky Transparency of Sky
-		* @param float $airMass airMass during the observation
-		* @param String $filter Filter selected
-		* @param String $moonPhase moon phase selected
-		* @param float $q Quantum Efficiency
-		* @param float $f0 flux of a zero magnitude source
-		* @param float $filterWidth FilterWidth
-		* @param float $effectiveLenght EffectiveWaveLenght of Filter
-		* @param float $dTel Telescope's Diameter
-		* @param float $plateScale Plate Scale of CCD
-		* @param float $fCalib fCalib
- 		* @param float $binning Binning of CCD.
+		* Constructor: It sets up attributes of class
+		* @param String $transparencySky - Transparency of Sky
+		* @param float $airMass - airMass during the observation
+		* @param String $filter - Filter selected
+		* @param String $moonPhase - moon phase selected
+		* @param float $q - Quantum Efficiency
+		* @param float $f0 - flux of a zero magnitude source
+		* @param float $filterWidth - FilterWidth
+		* @param float $effectiveLenght - EffectiveWaveLenght of Filter
+		* @param float $dTel - Telescope's Diameter
+		* @param float $plateScale - Plate Scale of CCD
+		* @param float $fCalib - fCalib
+ 		* @param float $binning - Binning of CCD.
 		*/
 		function __construct($transparencySky, $airMass , $filter, $moonPhase , $q , $f0, $filterWidth, $effectiveLenght, $dTel, $plateScale, $fCalib, $binning, $fArea, $tTel,$tInstr, $tFilter)
 		{
@@ -40,16 +40,16 @@
 			$this->setNumberPhotons($this->getMagnitudeSky(), $this->getTransparencySky(), $q, $f0, $filterWidth, $effectiveLenght, $dTel, $plateScale, $binning, $fArea, $tTel,$tInstr, $tFilter);
 		}
 		/**
-		* Sets up Number Photons of Sky
-		* @param float $mSky magnitude of Sky
-		* @param float $tSky Transparency of Sky
-		* @param float $q Quantum Efficiency
-		* @param float $f0 flux of a zero magnitude source
-		* @param float $filterWidth FilterWidth
-		* @param float $effectiveLenght EffectiveWaveLenght of Filter
-		* @param float $dTel Telescope's Diameter
-		* @param float $plateScale Plate Scale of CCD
- 		* @param float $binning Binning of CCD.
+		* It sets up the Number Photons of Sky
+		* @param float $mSky - magnitude of Sky
+		* @param float $tSky - Transparency of Sky
+		* @param float $q - Quantum Efficiency
+		* @param float $f0 - flux of a zero magnitude source
+		* @param float $filterWidth - FilterWidth
+		* @param float $effectiveLenght - EffectiveWaveLenght of Filter
+		* @param float $dTel - Telescope's Diameter
+		* @param float $plateScale - Plate Scale of CCD
+ 		* @param float $binning - Binning of CCD.
 		*/
 		public function setNumberPhotons($mSky, $tSky, $q , $f0, $filterWidth, $effectiveLenght, $dTel, $plateScale, $binning, $fArea, $tTel,$tInstr, $tFilter)
 		{
@@ -57,57 +57,57 @@
 				$this->numberPhotons = $n;
 		}
 		/**
-		* Return numberPhotons 
-		* @return float numberPhotons
+		* It returns the numberPhotons 
+		* @return float $numberPhotons - number photons of Sky
 		*/
 		public function getNumberPhotons()
 		{
 			return $this->numberPhotons;
 		}
 		/**
-		* Sets up Transparency of Sky 
-		* @param float $k coefficient of extinction
-		* @param float $x air mass
+		* It sets up the Transparency of Sky 
+		* @param float $k - coefficient of extinction
+		* @param float $x - air mass
 		*/
 		public function setTransparencySky($k , $x)
 		{
 			$this->transparencySky = pow(10,-0.4*$k*$x);
 		}
 		/**
-		* Return Transparency of Sky
-		* @return float $transparencySky
+		* It returns the Transparency of Sky
+		* @return float $transparencySky - transparency of sky
 		*/
 		public function getTransparencySky()
 		{
 			return $this->transparencySky;
 		}
 		/**
-		* Sets up Magnitude of Sky
-		* @param float $magnitude magnitude of sky
+		* It sets up the Magnitude of Sky
+		* @param float $magnitude - magnitude of sky
 		*/
 		public function setMagnitudeSky($magnitude)
 		{
 			$this->magnitudeSky = $magnitude;
 		}
 		/**
-		* Return Magnitude of Sky
-		* @return float $magnitude magnitude of sky
+		* It returns the Magnitude of Sky
+		* @return float $magnitude - magnitude of sky
 		*/
 		public function getMagnitudeSky()
 		{
 			return $this->magnitudeSky;
 		}
 		/**
-		* Set FCablib
-		* @param $value Fcalib
+		* It's set up the FCablib
+		* @param $value - Fcalib
 		*/
 		public function setFcalib($value)
 	 	{
 	 		$this->fCalib = $value;
 	 	}
 	 	/**
-		* Return Fcalib
-		* @return Fcalib
+		* It returns the Fcalib
+		* @return float $fCalib - fCalib
 		*/
 	 	public function getFcalib()
 	 	{
